0194cfb77603ea8b0151621b503d5cbbc8ee4111
Alvin Aurestrand <alvin.aurestrand@guteskolan.se>
2025-03-28 14:22:56 +0100

    update

--- Assets/Scripts/PlayerScripts/Damageable.cs
+++ Assets/Scripts/PlayerScripts/Damageable.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Damageable : MonoBehaviour
 {
 
     
     public int health = 100;
+
+    public float invincibilityTime = 0.5f;
+    float clock = 0f;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Something hit me!");
-        health -= 10;
-        Debug.Log(health);
-        if (health <= 0)
+
+        if (Time.time > clock)
         {
-            Debug.Log("Dead");
-            Application.Quit();
-            UnityEditor.EditorApplication.isPlaying = false;
+            
+            Debug.Log("Something hit me!");
+            health -= 10;
+            Debug.Log(health);
+            clock = Time.time + invincibilityTime;
+
+            if (health <= 0)
+            {
+                Debug.Log("Dead");
+                SceneManager.LoadScene("MainMenu");
+                /*Application.Quit();
+                UnityEditor.EditorApplication.isPlaying = false;*/
+            }
         }
     }
    
--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -8,22 +8,32 @@ public class MainMenuController : MonoBehaviour
 {
     VisualElement root;
     Button startButton;
+    Button quitButton;
 
     private void Awake()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
         startButton = root.Q<Button>("start-button");
         startButton.RegisterCallback<ClickEvent>(OnStartButtonClicked);
+
+        quitButton = root.Q<Button>("quit-button");
+        quitButton.RegisterCallback<ClickEvent>(OnQuitButtonClicked);
     }
 
     private void OnDisable()
     {
         startButton.UnregisterCallback<ClickEvent>(OnStartButtonClicked);
+        quitButton.UnregisterCallback<ClickEvent>(OnQuitButtonClicked);
     }
 
     void OnStartButtonClicked(ClickEvent evt)
     {
         print("Start button was clicked");
         SceneManager.LoadScene("MainGame");
+    }
+
+    void OnQuitButtonClicked(ClickEvent evt) {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
